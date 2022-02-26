ff8e6931ab48c14f8516928c8c6219e2597d4eb2
ellismcdougald <ellismcdougald@icloud.com>
2022-02-26 09:29:04 -0700

    Add event listeners

--- javascript.js
+++ javascript.js
@@ -1,14 +1,18 @@
+//Creating grid
 const rows = document.querySelectorAll('.row');
-console.log(rows);
 let arr = [...rows];
-console.log(arr);
-
-//arr[1].appendChild(square);
 
 for(let i=0; i<16; i++) {
     for(let j=0; j<7; j++) {
         const square = document.createElement('div');
         square.classList.add('square');
+        square.addEventListener('mouseenter', () => {
+            square.classList.add('enter');
+            console.log('hover');
+        });
         arr[i].appendChild(square);
     }
 }
+
+//Hover effect
+
