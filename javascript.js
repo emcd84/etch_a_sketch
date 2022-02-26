a89ff1e1a642aebedf4d013772a4674459ca0e94
ellismcdougald <ellismcdougald@icloud.com>
2022-02-26 09:54:32 -0700

    Add custom grid functionality

--- javascript.js
+++ javascript.js
@@ -1,9 +1,34 @@
-//Creating grid
-const rows = document.querySelectorAll('.row');
-let arr = [...rows];
+function empty(element) {
+    element.replaceChildren();
+}
 
-for(let i=0; i<16; i++) {
-    for(let j=0; j<7; j++) {
+//Button
+let gridSize = 0;
+const btn = document.querySelector('button');
+console.log(btn);
+btn.addEventListener('click', () => {
+    
+    gridSize = prompt("Enter the number of squares per side of your grid:");
+    if(gridSize > 100) {
+        gridSize = 100;
+    }
+    const container = document.querySelector('#container');
+    empty(container);
+
+    //Add rows
+    for(let i=0; i<gridSize; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+    }
+
+    //Get array of row elements
+    const rows = document.querySelectorAll('.row');
+    let arr = [...rows];
+
+    //Add squares
+    for(let i=0; i<gridSize; i++) {
+    for(let j=0; j<gridSize; j++) {
         const square = document.createElement('div');
         square.classList.add('square');
         square.addEventListener('mouseenter', () => {
@@ -13,6 +38,10 @@ for(let i=0; i<16; i++) {
         arr[i].appendChild(square);
     }
 }
+})
+console.log(gridSize);
 
-//Hover effect
+//Creating grid
+
+
 
